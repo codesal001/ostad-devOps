69eac02ceceeef8fff6713e0859c3a9536785cca
codesal001 <salin.ak.shahi1995@gmail.com>
2025-08-24 01:16:38 +0200

    add more use case

--- closure.js
+++ closure.js
@@ -4,11 +4,15 @@ function makeCounter() {
     count++;
     return count;
   }
+  function decrement() {
+    count--;
+    return count;
+  }
   function reset() {
     count = 0;
     return count;
   }
-  return { increment, reset };
+  return { increment, decrement, reset };
 }
 
 let counter = makeCounter();
@@ -16,5 +20,29 @@ let counter = makeCounter();
 console.log(counter.increment());
 console.log(counter.increment());
 console.log(counter.increment());
+console.log(counter.decrement());
 console.log(counter.reset());
 console.log(counter.increment());
+
+function Account(initialBalance) {
+  balance = initialBalance;
+  function deposite(amount) {
+    balance = balance + amount;
+    return balance;
+  }
+  function withdraw(amount) {
+    if (amount > balance) {
+      console.log("Insufficent Balance");
+      return;
+    }
+    balance = balance - amount;
+    return balance;
+  }
+
+  return { deposite, withdraw };
+}
+
+const myAccount = Account(5000);
+
+console.log(myAccount.deposite(1000));
+console.log(myAccount.withdraw(2000));
